306b300a809cf59eff8f318fc6380397458375b0
Sarah Kendrew <sarahaskendrew@gmail.com>
2021-03-02 10:39:57 -0500

    updated filename

--- wasp43b_exonoodle.py
+++ wasp43b_exonoodle.py
@@ -7,6 +7,6 @@ Run a exoNoodle simulation for a given exonoodle spectrum
 import exonoodle
 
 #Initialisation of the variables
-filename = "/Users/kendrew/sci_projects/exoplanets-ers/Webb-ERS-simulations/wasp43b_config.ini" # The filename with all the characteristics of your system
+filename = "/Users/kendrew/sci_projects/exoplanets-ers/Webb-ERS-simulations/wasp43b_bb_noLD_config.ini" # The filename with all the characteristics of your system
 
 exonoodle.run(filename)
